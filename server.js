d9a91ac35824ce6ab5837860e7b42c0f88bc151a
Drozerah <drozerah@gmail.com>
2020-04-13 17:01:14 +0200

    Set up a Template Engine

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express     = require('express')
 const bodyParser  = require('body-parser')
 const fccTesting  = require('./freeCodeCamp/fcctesting.js')
+const path = require('path')
 
 const app = express()
 
@@ -10,12 +11,23 @@ fccTesting(app) //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'pug')
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html')
-  })
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT)
+// Home page
+app.get('/', async(req, res, next) => {
+    try {
+      const view = await path.join(process.cwd(), '/views/pug/index.pug')
+      res.render(view)
+    } catch (error) {
+      next(error)
+    }
 })
+
+// Route not found (404)
+app.use((req, res, next) => res.status(404).type("text").send("Not Found"))
+
+// 500 - Any server error
+app.use((err, req, res, next) => res.status(500).send({ error: err }))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log("Listening on port " + PORT))
